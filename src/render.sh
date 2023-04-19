49a0e07bf474b4530dad9bdd339826499fdbc75c
Mike Carey <mike-carey@users.noreply.github.com>
2023-04-19 16:18:31 -0500

    Properly handles multline strings for content

--- src/render.sh
+++ src/render.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+function set-output() {
+  if [ -f "$OUTPUT" ]; then
+    echo "file=$OUTPUT" >> $GITHUB_OUTPUT
+
+    {
+      echo "content=<<EOF"
+      cat "$OUTPUT"
+      echo "EOF"
+    } >> "$GITHUB_OUTPUT"
+  fi
+
+  echo "[DEBUG] Output file not found, skipping outputs" >&2
+}
+
 function render() {
   set -eo pipefail
 
@@ -25,27 +39,7 @@ function render() {
   }
 
   # Main
-
-  function set_output() {
-    if [ -f "$OUTPUT" ]; then
-      echo "file=$OUTPUT" >> $GITHUB_OUTPUT
-
-      CONTENT="$(< "$OUTPUT")"
-
-      echo "[DEBUG] Condensing $(wc -l "$OUTPUT") lines" >&2
-
-      # Multiline variables need special treatment
-      # @see https://trstringer.com/github-actions-multiline-strings/
-      CONTENT="${CONTENT//'%'/'%25'}"
-      CONTENT="${CONTENT//$'\n'/'%0A'}"
-      CONTENT="${CONTENT//$'\r'/'%0D'}"
-
-      echo -n "content=$CONTENT" >> $GITHUB_OUTPUT
-    fi
-
-    echo "[DEBUG] Output file not found, skipping outputs" >&2
-  }
-  trap set_output EXIT
+  trap set-output EXIT
 
   local COMMAND=(j2 -o "$OUTPUT")
 
